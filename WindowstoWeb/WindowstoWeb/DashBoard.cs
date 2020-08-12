9898a64216ff230008e99c9177d983ac37771e80
Dhinesh Viswa <dhineshviswa@outlook.com>
2020-08-12 21:31:37 +0530

    Gmail Signin Redirect

--- WindowstoWeb/WindowstoWeb/DashBoard.cs
+++ WindowstoWeb/WindowstoWeb/DashBoard.cs
@@ -23,7 +23,10 @@ namespace WindowstoWeb
         {
             try
             {
-
+                if (txtPath.Text.Trim() == "")
+                    System.Diagnostics.Process.Start("https://mail.google.com/mail/u/0/?tab=wm#inbox");
+                else
+                    System.Diagnostics.Process.Start("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
             }
             catch (Exception ex)
             {
